03f864a397ab76b516f906f84af8351dbf7a15e8
Petrov Dmitriy <Dmitriy.Petrov@hofftech.ru>
2026-03-31 21:12:27 +0300

    Откат домашнего задания 2, для работы с третьим домашним заданием

--- src/main/java/ru/hofftech/importparcel/model/dto/ImportParcelInvalidDto.java
+++
@@ -1,8 +0,0 @@
-package ru.hofftech.importparcel.model.dto;
-
-import lombok.Builder;
-import org.jspecify.annotations.NonNull;
-import ru.hofftech.shared.model.dto.ParcelDto;
-
-@Builder
-public record ImportParcelInvalidDto(@NonNull ParcelDto parcel, @NonNull String cause) {}
--- src/main/java/ru/hofftech/shared/exception/JsonUtilException.java
+++
@@ -1,9 +0,0 @@
-package ru.hofftech.shared.exception;
-
-import org.jspecify.annotations.NonNull;
-
-public class JsonUtilException extends RuntimeException {
-    public JsonUtilException(@NonNull String message, @NonNull Throwable cause) {
-        super(message, cause);
-    }
-}
